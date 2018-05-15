d79124dae6fec8d995355fff3770b6d8bf1cd1b1
Leszek Bochenek <lechu45@gmail.com>
2018-05-15 23:29:16 +0200

    Version 2.0

--- ConsoleMenu/ConsoleMenu.cs
+++ ConsoleMenu/ConsoleMenu.cs
@@ -1,84 +1,115 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleTools
 {
-  public static class ConsoleMenu
+  public class ConsoleMenu
   {
-    public static void Show(params (string name, Action action)[] menuItems)
+    private readonly MenuConfig Config = new MenuConfig();
+    private readonly List<Tuple<string, Action>> _menuItems = new List<Tuple<string, Action>>();
+
+    public ConsoleMenu Add(string name, Action action)
     {
-      Show(null, menuItems);
+      _menuItems.Add(new Tuple<string, Action>(name, action));
+      return this;
     }
 
-    public static void Show(MenuConfig config = null, params (string name, Action action)[] menuItems)
+    public ConsoleMenu AddRange(IEnumerable<Tuple<string, Action>> menuItems)
     {
-      config = config ?? new MenuConfig();
+      _menuItems.AddRange(menuItems);
+      return this;
+    }
+
+    public ConsoleMenu Configure(Action<MenuConfig> configure)
+    {
+      configure?.Invoke(this.Config);
+      return this;
+    }
+
+    public static void Close() => throw new InvalidOperationException("Don't run this method directly. Just pass a reference to this method.");
+
+    public void Show()
+    {
       ConsoleKeyInfo key;
+      var menuItems = _menuItems.ToArray();
       int curItem = 0;
       var currentForegroundColor = Console.ForegroundColor;
       var currentBackgroundColor = Console.BackgroundColor;
       bool breakIteration = false;
-      do
+      while (true)
       {
-        if (config.ClearConsole)
+        do
         {
-          Console.Clear();
-        }
-        config.WriteHeaderAction();
-
-        for (int i = 0; i < menuItems.Length; i++)
-        {
-          if (curItem == i)
+          if (Config.ClearConsole)
           {
-            Console.BackgroundColor = config.SelectedItemBackgroundColor;
-            Console.ForegroundColor = config.SelectedItemForegroundColor;
-            Console.Write(config.Selector);
-            config.WriteItemAction((menuItems[i].name, i));
-            Console.WriteLine();
-            Console.BackgroundColor = config.ItemBackgroundColor;
-            Console.ForegroundColor = config.ItemForegroundColor;
+            Console.Clear();
           }
-          else
+          Config.WriteHeaderAction();
+
+          for (int i = 0; i < menuItems.Length; i++)
           {
-            Console.BackgroundColor = config.ItemBackgroundColor;
-            Console.ForegroundColor = config.ItemForegroundColor;
-            Console.Write(new string(' ', config.Selector.Length));
-            config.WriteItemAction((menuItems[i].name, i));
-            Console.WriteLine();
+            var itemName = menuItems[i].Item1;
+            if (curItem == i)
+            {
+              Console.BackgroundColor = Config.SelectedItemBackgroundColor;
+              Console.ForegroundColor = Config.SelectedItemForegroundColor;
+              Console.Write(Config.Selector);
+              Config.WriteItemAction(new MenuItem { Name = itemName, Index = i });
+              Console.WriteLine();
+              Console.BackgroundColor = Config.ItemBackgroundColor;
+              Console.ForegroundColor = Config.ItemForegroundColor;
+            }
+            else
+            {
+              Console.BackgroundColor = Config.ItemBackgroundColor;
+              Console.ForegroundColor = Config.ItemForegroundColor;
+              Console.Write(new string(' ', Config.Selector.Length));
+              Config.WriteItemAction(new MenuItem { Name = itemName, Index = i });
+              Console.WriteLine();
+            }
           }
-        }
 
-        if (breakIteration)
-        {
-          break;
-        }
+          if (breakIteration)
+          {
+            break;
+          }
 
-        readKey:
-        key = Console.ReadKey(true);
+          readKey:
+          key = Console.ReadKey(true);
 
-        if (key.Key == ConsoleKey.DownArrow)
-        {
-          curItem++;
-          if (curItem > menuItems.Length - 1) curItem = 0;
-        }
-        else if (key.Key == ConsoleKey.UpArrow)
-        {
-          curItem--;
-          if (curItem < 0) curItem = menuItems.Length - 1;
-        }
-        else if (key.KeyChar >= '0' && (key.KeyChar - '0') < menuItems.Length)
-        {
-          curItem = key.KeyChar - '0';
-          breakIteration = true;
-        }
-        else if (key.Key != ConsoleKey.Enter)
-        {
-          goto readKey;
-        }
-      } while (key.Key != ConsoleKey.Enter);
+          if (key.Key == ConsoleKey.DownArrow)
+          {
+            curItem++;
+            if (curItem > menuItems.Length - 1) curItem = 0;
+          }
+          else if (key.Key == ConsoleKey.UpArrow)
+          {
+            curItem--;
+            if (curItem < 0) curItem = menuItems.Length - 1;
+          }
+          else if (key.KeyChar >= '0' && (key.KeyChar - '0') < menuItems.Length)
+          {
+            curItem = key.KeyChar - '0';
+            breakIteration = true;
+          }
+          else if (key.Key != ConsoleKey.Enter)
+          {
+            goto readKey;
+          }
+        } while (key.Key != ConsoleKey.Enter);
 
-      Console.ForegroundColor = currentForegroundColor;
-      Console.BackgroundColor = currentBackgroundColor;
-      menuItems[curItem].action();
+        Console.ForegroundColor = currentForegroundColor;
+        Console.BackgroundColor = currentBackgroundColor;
+        var action = menuItems[curItem].Item2;
+        if (action == Close)
+        {
+          return;
+        }
+        else
+        {
+          action?.Invoke();
+        }
+      }
     }
   }
 
@@ -89,8 +120,10 @@ namespace ConsoleTools
     public ConsoleColor ItemBackgroundColor = Console.BackgroundColor;
     public ConsoleColor ItemForegroundColor = Console.ForegroundColor;
     public Action WriteHeaderAction = () => Console.WriteLine("Pick an option:");
-    public Action<(string itemName, int itemIndex)> WriteItemAction = item => Console.Write("[{0}] {1}", item.itemIndex, item.itemName);
+    public Action<MenuItem> WriteItemAction = item => Console.Write("[{0}] {1}", item.Index, item.Name);
     public string Selector = ">> ";
     public bool ClearConsole = true;
   }
+
+  public struct MenuItem { public string Name; public int Index; };
 }
--- ConsoleMenuSampleApp/Program.cs
+++ ConsoleMenuSampleApp/Program.cs
@@ -7,15 +7,30 @@ namespace ConsoleMenuSampleApp
   {
     static void Main(string[] args)
     {
-      ConsoleMenu.Show(
-        new MenuConfig { },
-        ("One", () => Console.WriteLine("One")),
-        ("Two", () => Console.WriteLine("Two")),
-        ("Three", () => Console.WriteLine("Three")),
-        ("Four", () => Console.WriteLine("Four")),
-        ("Close", () => { }),
-        ("Exit", () => Environment.Exit(0)));
+      var subMenu = new ConsoleMenu()
+        .Add("Sub_One", () => SomeAction("Sub_One"))
+        .Add("Sub_Two", () => SomeAction("Sub_Two"))
+        .Add("Sub_Three", () => SomeAction("Sub_Three"))
+        .Add("Sub_Four", () => SomeAction("Sub_Four"))
+        .Add("Sub_Close", ConsoleMenu.Close)
+        .Add("Sub_Exit", () => Environment.Exit(0));
 
+      var menu = new ConsoleMenu()
+        .Add("One", () => SomeAction("One"))
+        .Add("Two", () => SomeAction("Two"))
+        .Add("Three", () => SomeAction("Three"))
+        .Add("Four", subMenu.Show)
+        .Add("Close", ConsoleMenu.Close)
+        .Add("Exit", () => Environment.Exit(0))
+        .Configure(config => { config.Selector = "--> "; });
+
+      menu.Show();
+    }
+
+    private static void SomeAction(string text)
+    {
+      Console.WriteLine(text);
+      Console.WriteLine("Press any key to continue...");
       Console.ReadKey();
     }
   }
